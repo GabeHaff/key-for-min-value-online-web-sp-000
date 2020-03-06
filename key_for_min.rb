530b7b1d7c7d58e2f4535237d2196bda6fbe57c1
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-03-06 14:25:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,16 +2,3 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(nutritious_hash)
-named_hash={:blake => 12, :ashley => 7,:adam => 5}
-index = 0 
-named_hash.each do |key, value| 
-  if value[index] < value[index+1]
-    min_key=value 
-     index= index+1 
-    key_for_min_value[value]
-   else
-    return nil
-
-end 
-end 
-end 

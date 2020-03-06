59e8803d919f592deae771ea7dc6c5a229ed8215
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-03-06 14:33:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,14 @@
 
 def key_for_min_value(named_hash)
 named_array = named_hash.to_a
-
+default_value=lowest_key_value
   default_value= 1000
   default_key_value= nil 
-
 
   named_array.each do |i|
       if i[1] < default_value
         default_value = i[1]
-        default_key_value = i[0]
+        lowest_key_value = i[0]
       end
    end
   return default_key_value

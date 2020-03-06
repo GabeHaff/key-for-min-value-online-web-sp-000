298e46b147fffefb9af0039e6211c5eeebfe41d7
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-03-06 14:16:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(nutritious_hash)
+nutritious_hash={:apple => 12, :broccoli => 7,:carrot => 5}
+index = 0 
+nutritious_hash.each do |key, value| 
+  if value[index] < value[index+1]
+    min_key=value 
+    index= index+1 
+    key_for_min_value[value]
+    elsif nutritious_hash
 
-end
+return min_key 
+end 
+end 
+end 
